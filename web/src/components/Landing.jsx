7828903f72cfcabb586523e5070665c4fea19d7d
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-10-10 11:56:53 +0200

    Update Landing.jsx

--- web/src/components/Landing.jsx
+++ web/src/components/Landing.jsx
@@ -19,7 +19,7 @@ function Landing({ projectsArray }) {
             <div className="card__wrap">
             {projectsArray.map((project) => {
                 return (
-                    <a href={`https://module-4-promo-bpt-proyectos-molones.onrender.com/projects/${project.idproject}`} key={project.idproject} className="detail-link">
+                    <a href={project.idproject} key={project.idproject} className="detail-link">
                         <article className="card" key={project.idproject}>
                         <h2 className="card__projectTitle">
                             <span className="card__projectTitle--text">Personal project card</span>
